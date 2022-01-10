a875a9db7ce11807fcaccce7609d66fa8e9684e0
Rauli Aaltonen <rauli.aaltonen@student.careeria.fi>
2022-01-10 12:58:58 +0200

    Koira esimerkki

---
+++ Olio ohjelmointi/Test app/Koira.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test_app
+{
+    class Koira
+    {
+        public string nimi;
+        public int ikä;
+        public string rotu;
+
+        public void TulostaData()
+        {
+            Console.WriteLine("Koiran nimi: " + nimi + ", Ikä: " + ikä + ", Rotu:" + rotu);
+        }
+
+        public void Hauku()
+
+        {
+            Console.WriteLine("Woof Woof");
+        }
+    }
+}
