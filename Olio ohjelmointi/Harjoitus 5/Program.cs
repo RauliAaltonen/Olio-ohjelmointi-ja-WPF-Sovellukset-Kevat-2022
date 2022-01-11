4ab39f630420a54fa5ba2ccbb62a499f7a1c6292
Rauli Aaltonen <rauli.aaltonen@student.careeria.fi>
2022-01-11 10:16:06 +0200

    Kotitehtävä 5

--- Olio ohjelmointi/Harjoitus 5/Program.cs
+++ Olio ohjelmointi/Harjoitus 5/Program.cs
@@ -2,11 +2,19 @@
 
 namespace Harjoitus_5
 {
+
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            float a = 15;
+            float b = 30;
+
+            Console.WriteLine(Laskin.Summa(a, b));
+            Console.WriteLine(Laskin.Erotus(a, b));
+            Console.WriteLine(Laskin.Kertolasku(a, b));
+            Console.WriteLine(Laskin.Jakolasku(a, b));
         }
-    }
+    }   
 }
